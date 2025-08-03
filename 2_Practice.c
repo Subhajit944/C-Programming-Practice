767d0770ff23f2a044dd90949f27e8ea9b18c444
Subhajit Das <subhajit9004@gmail.com>
2025-08-03 23:20:34 +0530

    Add files via upload

---
+++ 2_Practice.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+
+    int age = 22 ;
+    float pi = 3.14;
+    char star = '*';
+
+
+    printf("age id  %d \n", age);
+    printf("value of pi is %f \n", pi);
+    printf("star looks like this  %c \n",  star);
+
+    return 0;
+}
