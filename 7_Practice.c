793a89a8f3e2a6fadb43ae9499f8ac5772b74e5d
Subhajit Das <subhajit9004@gmail.com>
2025-08-05 23:20:44 +0530

    Add files via upload

---
+++ 7_Practice.c
@@ -0,0 +1,14 @@
+//Arithmetic Instructions....
+
+#include<stdio.h>
+
+int main() {
+
+    int a = 1, b = 2;
+    int sum = a + b;
+    int multiply = a * b;
+
+    printf(" sum of the number : %d \n" , sum);
+    printf(" multiply of the number : %d \n" , multiply);
+    return 0;
+}
