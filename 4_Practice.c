9caa0db6c76b3b36b8f56e5e10db1b90bf444eff
Subhajit Das <subhajit9004@gmail.com>
2025-08-05 23:19:50 +0530

    Add files via upload

---
+++ 4_Practice.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//area of square
+int main(){
+
+    int side;
+
+    printf("enter side");
+    scanf("%d", &side);
+
+    int area = side * side;
+    printf("area is : %d", area);
+    
+    return 0;
+}
