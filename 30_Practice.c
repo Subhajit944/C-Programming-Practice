6518c29c66a1eb7da142b48de16476ab304474c6
Subhajit Das <subhajit9004@gmail.com>
2025-08-12 23:24:43 +0530

    Add files via upload

---
+++ 30_Practice.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+
+    int marks;
+
+    printf("Enter number(0-100) :  ");
+    scanf("%d", &marks);
+
+    if(marks >= 0 && marks <= 30) {
+
+        printf("FAIL \n");
+    
+    }else if (marks > 30 && marks <= 100){
+
+        printf("PASS \n");
+
+    }else{
+
+        printf("wrong marks");
+    }
+    
+    return 0;
+}
