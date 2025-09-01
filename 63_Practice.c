eacb65b4c1eda92dae2e0fa02d991314e241a977
Subhajit Das <subhajit9004@gmail.com>
2025-09-01 22:42:23 +0530

    Add files via upload

---
+++ 63_Practice.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+float rectangleArea(float a , float b);
+
+int main() {
+
+    float a = 5.0;
+    float b = 10.0;
+    printf("area  is : %f", rectangleArea(a,b));
+    return 0;
+
+}
+
+float rectangleArea(float a , float b) {
+    return a * b;
+}
