d7f1648186d7bcc1281ddff656e1478564f8c403
Subhajit Das <subhajit9004@gmail.com>
2025-09-12 22:24:32 +0530

    Add files via upload

---
+++ 76_Practice.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void swap(int a, int b);
+
+int main(){
+
+    int a = 3, b = 5;
+    swap(a,b);
+    return 0;
+}
+
+void swap(int a, int b){
+
+    int t = a;
+    a = b;
+    b = t;
+    
+    printf("a = %d  & b = %d\n", a, b );
+}
