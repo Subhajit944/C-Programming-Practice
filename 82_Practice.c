81a9157934e1135a08e4ee2b3a2b70d2abe453a7
Subhajit Das <subhajit9004@gmail.com>
2025-09-17 21:59:37 +0530

    Add files via upload

---
+++ 82_Practice.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+    char star = '*';
+    char *ptr = &star;
+
+    printf("ptr = %u\n", ptr);
+    ptr++;
+
+    printf("ptr = %u\n", ptr);
+    ptr--;
+
+    printf("ptr = %u\n", ptr);
+    
+    return 0;
+}
