ed3ed4a8c709b30d4c770870545f191acff9ce87
Subhajit Das <subhajit9004@gmail.com>
2025-09-19 18:46:27 +0530

    Add files via upload

---
+++ 83_Practice.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+    int age = 22;
+    int  *ptr = &age;
+
+    printf("ptr = %u\n", ptr);
+    ptr++;
+
+    printf("ptr = %u\n", ptr);
+    ptr--;
+
+    printf("ptr = %u\n", ptr);
+    
+    return 0;
+}
